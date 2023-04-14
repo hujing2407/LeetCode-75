57c381abf8f540321aaf8a02a31e8b121fb693e2
Jing Hu <hu000095@algonquinlive.com>
2023-04-14 17:25:38 -0400

    Add easy problem 66

--- Easy/src/Test.java
+++ Easy/src/Test.java
@@ -1,23 +1,31 @@
+import java.security.PublicKey;
+
 /**
  * @author Jing Hu
  * @date Apr. 13 2023
  */
 public class Test {
     public static void main(String[] args) {
-//        String s = "III";
-//        RomanToInteger13 roman = new RomanToInteger13();
-//        System.out.println(roman.romanToInt(s));
+        /*
+        String s = "III";
+        RomanToInteger13 roman = new RomanToInteger13();
+        System.out.println(roman.romanToInt(s));
 
-//        int nums[] = {3,2,2,3};
-//        RemoveElement27 removeElement27 = new RemoveElement27();
-//        int index = removeElement27.removeElement(nums, 3);
-//        for (int i = 0; i < index; i++) {
-//            System.out.println(nums[i]);
-//        }
+        int nums[] = {3,2,2,3};
+        RemoveElement27 removeElement27 = new RemoveElement27();
+        int index = removeElement27.removeElement(nums, 3);
+        for (int i = 0; i < index; i++) {
+            System.out.println(nums[i]);
+        }
 
         int nums[] = {1};
         SearchInsertPosition35 searchInsertPosition35 = new SearchInsertPosition35();
         int i = searchInsertPosition35.searchInsert(nums, 1);
         System.out.println(i);
+        */
+
+        int nums[] = {9,9};
+        PlusOne66 plusOne66 = new PlusOne66();
+        plusOne66.plusOne(nums);
     }
 }
