398c75aaef6b082b810684e785f6b169d2b87952
Jing Hu <hu000095@algonquinlive.com>
2023-04-15 22:39:26 -0400

    Excel title

---
+++ Easy/src/Array/Test.java
@@ -0,0 +1,12 @@
+package Array;
+
+/**
+ * @author Jing Hu
+ * @date Apr. 15 2023
+ */
+public class Test {
+  public static void main(String[] args) {
+    ExcelSheetColumnTitle168 excelSheetColumnTitle168 = new ExcelSheetColumnTitle168();
+    excelSheetColumnTitle168.convertToTitle(701);
+  }
+}
