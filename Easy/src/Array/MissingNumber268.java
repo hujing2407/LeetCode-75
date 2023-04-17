99dcacde0f12f2f2c4982c9c579cf32afff36f10
Jing Hu <hu000095@algonquinlive.com>
2023-04-17 16:41:39 -0400

    math @Apr 17,2023

--- Easy/src/Array/MissingNumber268.java
+++ Easy/src/Array/MissingNumber268.java
@@ -26,3 +26,23 @@ public class MissingNumber268 {
     return diff;
   }
 }
+
+class NumArray {
+  int[] n;
+  public NumArray(int[] nums) {
+    n=new int[nums.length];
+    n[0]=nums[0];
+    for(int i=1;i<nums.length;i++){
+      n[i]=nums[i]+n[i-1];
+    }
+  }
+
+  public int sumRange(int left, int right) {
+    if(left==0){
+      return n[right];
+    }
+    else{
+      return n[right]-n[left-1];
+    }
+  }
+}
