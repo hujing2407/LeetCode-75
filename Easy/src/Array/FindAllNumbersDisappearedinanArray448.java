92bc51ff7f346783ab4b9520fb439fbcb53eb610
Jing Hu <hu000095@algonquinlive.com>
2023-05-09 11:07:11 -0400

    448

---
+++ Easy/src/Array/FindAllNumbersDisappearedinanArray448.java
@@ -0,0 +1,25 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Jing Hu
+ * @date May 09 2023
+ */
+public class FindAllNumbersDisappearedinanArray448 {
+  public List<Integer> findDisappearedNumbers(int[] nums) {
+
+    ArrayList<Integer> list = new ArrayList<>();
+    int[] index = new int[nums.length + 1];
+    for (int i = 0; i < nums.length; i++) {
+      index[nums[i]]++;
+    }
+    for (int i = 1; i < index.length; i++) {
+      if (index[i] == 0) {
+        list.add(i);
+      }
+    }
+    return list;
+  }
+}
