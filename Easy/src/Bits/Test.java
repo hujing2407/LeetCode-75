5b2ea1f559f979c3075c180e49f39eae4effee7c
Jing Hu <hu000095@algonquinlive.com>
2023-05-10 15:21:09 -0400

    476

--- Easy/src/Bits/Test.java
+++ Easy/src/Bits/Test.java
@@ -8,7 +8,13 @@ import Bits.ExcelSheetColumnTitle168;
  */
 public class Test {
   public static void main(String[] args) {
+    /*
     ExcelSheetColumnTitle168 excelSheetColumnTitle168 = new ExcelSheetColumnTitle168();
     excelSheetColumnTitle168.convertToTitle(701);
+
+     */
+
+    NumberComplement476 numberComplement476 = new NumberComplement476();
+    numberComplement476.findComplement(5);
   }
 }
