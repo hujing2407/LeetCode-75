ee026f423416387b37634207ecf86cf0ad915491
Jing Hu <hu000095@algonquinlive.com>
2023-08-25 11:53:02 -0400

    Interview 150 Easy

--- Easy/src/Tree/ConvertSortedArraytoBinarySearchTree108.java
+++ Easy/src/Tree/ConvertSortedArraytoBinarySearchTree108.java
@@ -5,6 +5,7 @@ package Tree;
  * @date Apr. 15 2023
  */
 public class ConvertSortedArraytoBinarySearchTree108 {
+  /*
   public TreeNode sortedArrayToBST(int[] nums) {
     TreeNode root = new TreeNode(nums[0]);
     for (int i = 1; i < nums.length; i++) {
@@ -30,6 +31,22 @@ public class ConvertSortedArraytoBinarySearchTree108 {
         insertToBST(root.right,val);
       }
     }
+  }*/
+
+  public TreeNode sortedArrayToBST(int[] nums) {
+    return CreateBST(nums, 0, nums.length - 1);
   }
+
+  private TreeNode CreateBST(int[] nums, int left, int right) {
+    if (left > right) return null;
+
+    int mid = (left + right) / 2;
+    TreeNode root = new TreeNode(nums[mid]);
+    root.left = CreateBST(nums, left, mid - 1);
+    root.right = CreateBST(nums, mid + 1, right);
+
+    return root;
+  }
+
 
 }
