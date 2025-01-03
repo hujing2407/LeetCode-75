599489844983a61d168f4a3ba07f5fd73b1b0905
Jing Hu <hu000095@algonquinlive.com>
2025-01-03 17:52:56 -0500

    added bfs

--- src/python/00template.py
+++ src/python/00template.py
@@ -10,3 +10,29 @@ def dfs(node,visited):
     for next_node in node.children():
         if not next_node in visited:
             dfs(next_node,visited)
+
+def DFS(self,tree): # non-recursive version, using stack
+    if tree.root is None:
+        return []
+    visited,stack = [],[tree.root]
+    while stack:
+        node = stack.pop()
+        visited.add(node)
+
+        process(node)
+        nodes = generate_related_nodes(node)
+        stack.push(node)
+        # other processing work
+
+# BFS
+def BFS(graph,start,end):
+    queue = []
+    queue.append(start) 
+    visited.add(start)
+
+    while queue:
+        node = queue.pop()
+        visited.add(node)
+        process(node)
+        nodes = generate_related_nodes(node)
+        queue.push(node)
