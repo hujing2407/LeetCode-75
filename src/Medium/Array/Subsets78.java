d7e7eca360b7490fcbfb788362b797c3f220388e
Jing Hu <hu000095@algonquinlive.com>
2025-01-13 14:10:22 -0500

    78 subsets, backtrack()

--- src/Medium/Array/Subsets78.java
+++ src/Medium/Array/Subsets78.java
@@ -8,17 +8,34 @@ import java.util.List;
  * @date Mar 12 2021
  */
 public class Subsets78 {
+    List<List<Integer>> res = new LinkedList<>();
+    List<Integer> track = new LinkedList<>();
+
     public List<List<Integer>> subsets(int[] nums) {
-        List<Integer> list = new LinkedList<>();
-        LinkedList<List> subSets = new LinkedList<>();
+        backtrack(nums, 0);
+        return res;
+    }
+
+    private void backtrack(int[] nums, int start) {
+        res.add(new LinkedList<>(track));
+        for (int i = start; i < nums.length; i++) {
+            track.add(nums[i]);
+            backtrack(nums, i + 1);
+            track.removeLast();
+        }
+    }
+
+    public List<List<Integer>> subsets1(int[] nums) {
+        List<List<Integer>> list = new LinkedList<>();
+        list.add(new ArrayList<Integer>());
 
         for (int i = 0; i < nums.length; i++) { // list length = i
-            for (int j = 0; j <= i; j++) { //
-                for (int k = j; k < j; k++) {
-
-                }
+            for (int j = 0, size = list.size(); j < size; j++) { //
+                List<Integer> curr = new ArrayList<>(list.get(j));
+                curr.add(nums[i]);
+                list.add(curr);
             }
         }
-        return null;
+        return list;
     }
 }
